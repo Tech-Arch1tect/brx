7d496965d88f564e91b29c9d4146e9234b635152
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-08-19 23:15:01 +0100

    UserSession: increase token column data size

--- session/models.go
+++ session/models.go
@@ -7,7 +7,7 @@ import (
 type UserSession struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
 	UserID    uint      `json:"user_id" gorm:"not null;index"`
-	Token     string    `json:"token" gorm:"uniqueIndex;size:43;not null"`
+	Token     string    `json:"token" gorm:"uniqueIndex;size:255;not null"`
 	IPAddress string    `json:"ip_address" gorm:"size:45"`
 	UserAgent string    `json:"user_agent" gorm:"size:500"`
 	Current   bool      `json:"current" gorm:"-"`
