d142bc5d13f4e8b29756f60385e1fa1bd0966a71
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2026-01-19 23:44:35 +0000

    feat: add openapi documentation package

---
+++ openapi/openapi.go
@@ -0,0 +1,385 @@
+package openapi
+
+import (
+	"encoding/json"
+	"net/http"
+	"reflect"
+	"strings"
+	"sync"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/labstack/echo/v4"
+	"gopkg.in/yaml.v3"
+)
+
+type OpenAPI struct {
+	spec *openapi3.T
+	mu   sync.RWMutex
+}
+
+func New(title, version string) *OpenAPI {
+	spec := &openapi3.T{
+		OpenAPI: "3.0.3",
+		Info: &openapi3.Info{
+			Title:   title,
+			Version: version,
+		},
+		Paths:      openapi3.NewPaths(),
+		Components: &openapi3.Components{},
+	}
+
+	return &OpenAPI{
+		spec: spec,
+	}
+}
+
+func (o *OpenAPI) Description(desc string) *OpenAPI {
+	o.mu.Lock()
+	defer o.mu.Unlock()
+	o.spec.Info.Description = desc
+	return o
+}
+
+func (o *OpenAPI) TermsOfService(url string) *OpenAPI {
+	o.mu.Lock()
+	defer o.mu.Unlock()
+	o.spec.Info.TermsOfService = url
+	return o
+}
+
+func (o *OpenAPI) Contact(name, email, url string) *OpenAPI {
+	o.mu.Lock()
+	defer o.mu.Unlock()
+	o.spec.Info.Contact = &openapi3.Contact{
+		Name:  name,
+		Email: email,
+		URL:   url,
+	}
+	return o
+}
+
+func (o *OpenAPI) License(name, url string) *OpenAPI {
+	o.mu.Lock()
+	defer o.mu.Unlock()
+	o.spec.Info.License = &openapi3.License{
+		Name: name,
+		URL:  url,
+	}
+	return o
+}
+
+func (o *OpenAPI) Server(url, description string) *OpenAPI {
+	o.mu.Lock()
+	defer o.mu.Unlock()
+	o.spec.Servers = append(o.spec.Servers, &openapi3.Server{
+		URL:         url,
+		Description: description,
+	})
+	return o
+}
+
+func (o *OpenAPI) Tag(name, description string) *OpenAPI {
+	o.mu.Lock()
+	defer o.mu.Unlock()
+	o.spec.Tags = append(o.spec.Tags, &openapi3.Tag{
+		Name:        name,
+		Description: description,
+	})
+	return o
+}
+
+func (o *OpenAPI) BearerAuth(name, description string) *OpenAPI {
+	o.mu.Lock()
+	defer o.mu.Unlock()
+	o.ensureSecuritySchemes()
+	o.spec.Components.SecuritySchemes[name] = &openapi3.SecuritySchemeRef{
+		Value: &openapi3.SecurityScheme{
+			Type:         "http",
+			Scheme:       "bearer",
+			BearerFormat: "JWT",
+			Description:  description,
+		},
+	}
+	return o
+}
+
+func (o *OpenAPI) APIKeyAuth(name, paramName, location, description string) *OpenAPI {
+	o.mu.Lock()
+	defer o.mu.Unlock()
+	o.ensureSecuritySchemes()
+	o.spec.Components.SecuritySchemes[name] = &openapi3.SecuritySchemeRef{
+		Value: &openapi3.SecurityScheme{
+			Type:        "apiKey",
+			Name:        paramName,
+			In:          location,
+			Description: description,
+		},
+	}
+	return o
+}
+
+func (o *OpenAPI) CookieAuth(name, cookieName, description string) *OpenAPI {
+	return o.APIKeyAuth(name, cookieName, "cookie", description)
+}
+
+func (o *OpenAPI) OAuth2(name, description string, flows *openapi3.OAuthFlows) *OpenAPI {
+	o.mu.Lock()
+	defer o.mu.Unlock()
+	o.ensureSecuritySchemes()
+	o.spec.Components.SecuritySchemes[name] = &openapi3.SecuritySchemeRef{
+		Value: &openapi3.SecurityScheme{
+			Type:        "oauth2",
+			Description: description,
+			Flows:       flows,
+		},
+	}
+	return o
+}
+
+func (o *OpenAPI) ensureSecuritySchemes() {
+	if o.spec.Components.SecuritySchemes == nil {
+		o.spec.Components.SecuritySchemes = make(openapi3.SecuritySchemes)
+	}
+}
+
+func (o *OpenAPI) AddSchema(name string, example any) *OpenAPI {
+	o.mu.Lock()
+	defer o.mu.Unlock()
+	if o.spec.Components.Schemas == nil {
+		o.spec.Components.Schemas = make(openapi3.Schemas)
+	}
+	schema := generateSchema(example)
+	o.spec.Components.Schemas[name] = &openapi3.SchemaRef{Value: schema}
+	return o
+}
+
+func (o *OpenAPI) Spec() *openapi3.T {
+	o.mu.RLock()
+	defer o.mu.RUnlock()
+	return o.spec
+}
+
+func (o *OpenAPI) JSON() ([]byte, error) {
+	o.mu.RLock()
+	defer o.mu.RUnlock()
+	return json.MarshalIndent(o.spec, "", "  ")
+}
+
+func (o *OpenAPI) YAML() ([]byte, error) {
+	o.mu.RLock()
+	defer o.mu.RUnlock()
+
+	intermediate, err := o.spec.MarshalYAML()
+	if err != nil {
+		return nil, err
+	}
+	return yaml.Marshal(intermediate)
+}
+
+func (o *OpenAPI) JSONHandler() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		data, err := o.JSON()
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+		}
+		return c.JSONBlob(http.StatusOK, data)
+	}
+}
+
+func (o *OpenAPI) YAMLHandler() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		data, err := o.YAML()
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+		}
+		c.Response().Header().Set("Content-Type", "application/yaml")
+		return c.Blob(http.StatusOK, "application/yaml", data)
+	}
+}
+
+func (o *OpenAPI) SwaggerUIHandler(specPath string) echo.HandlerFunc {
+	html := `<!DOCTYPE html>
+<html>
+<head>
+    <title>API Documentation</title>
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css">
+</head>
+<body>
+    <div id="swagger-ui"></div>
+    <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+    <script>
+        SwaggerUIBundle({
+            url: "` + specPath + `",
+            dom_id: '#swagger-ui',
+            presets: [SwaggerUIBundle.presets.apis, SwaggerUIBundle.SwaggerUIStandalonePreset],
+            layout: "BaseLayout"
+        });
+    </script>
+</body>
+</html>`
+	return func(c echo.Context) error {
+		return c.HTML(http.StatusOK, html)
+	}
+}
+
+func (o *OpenAPI) Document(method, path string) *RouteBuilder {
+	return &RouteBuilder{
+		openapi:   o,
+		method:    method,
+		path:      path,
+		operation: &openapi3.Operation{Responses: openapi3.NewResponses()},
+	}
+}
+
+func (o *OpenAPI) addOperation(method, path string, op *openapi3.Operation) {
+	o.mu.Lock()
+	defer o.mu.Unlock()
+
+	openAPIPath := echoPathToOpenAPI(path)
+
+	pathItem := o.spec.Paths.Find(openAPIPath)
+	if pathItem == nil {
+		pathItem = &openapi3.PathItem{}
+		o.spec.Paths.Set(openAPIPath, pathItem)
+	}
+
+	switch strings.ToUpper(method) {
+	case http.MethodGet:
+		pathItem.Get = op
+	case http.MethodPost:
+		pathItem.Post = op
+	case http.MethodPut:
+		pathItem.Put = op
+	case http.MethodDelete:
+		pathItem.Delete = op
+	case http.MethodPatch:
+		pathItem.Patch = op
+	case http.MethodHead:
+		pathItem.Head = op
+	case http.MethodOptions:
+		pathItem.Options = op
+	}
+}
+
+func echoPathToOpenAPI(path string) string {
+	parts := strings.Split(path, "/")
+	for i, part := range parts {
+		if strings.HasPrefix(part, ":") {
+			parts[i] = "{" + strings.TrimPrefix(part, ":") + "}"
+		}
+	}
+	return strings.Join(parts, "/")
+}
+
+func generateSchema(example any) *openapi3.Schema {
+	if example == nil {
+		return &openapi3.Schema{Type: &openapi3.Types{"object"}}
+	}
+
+	return generateSchemaFromType(reflect.TypeOf(example))
+}
+
+func generateSchemaFromType(t reflect.Type) *openapi3.Schema {
+
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	switch t.Kind() {
+	case reflect.String:
+		return &openapi3.Schema{Type: &openapi3.Types{"string"}}
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return &openapi3.Schema{Type: &openapi3.Types{"integer"}}
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return &openapi3.Schema{Type: &openapi3.Types{"integer"}, Min: ptr(0.0)}
+	case reflect.Float32, reflect.Float64:
+		return &openapi3.Schema{Type: &openapi3.Types{"number"}}
+	case reflect.Bool:
+		return &openapi3.Schema{Type: &openapi3.Types{"boolean"}}
+	case reflect.Slice, reflect.Array:
+		return &openapi3.Schema{
+			Type:  &openapi3.Types{"array"},
+			Items: &openapi3.SchemaRef{Value: generateSchemaFromType(t.Elem())},
+		}
+	case reflect.Map:
+		return &openapi3.Schema{
+			Type: &openapi3.Types{"object"},
+			AdditionalProperties: openapi3.AdditionalProperties{
+				Schema: &openapi3.SchemaRef{Value: generateSchemaFromType(t.Elem())},
+			},
+		}
+	case reflect.Struct:
+		return generateStructSchema(t)
+	case reflect.Interface:
+		return &openapi3.Schema{Type: &openapi3.Types{"object"}}
+	default:
+		return &openapi3.Schema{Type: &openapi3.Types{"object"}}
+	}
+}
+
+func generateStructSchema(t reflect.Type) *openapi3.Schema {
+
+	if t.PkgPath() == "time" && t.Name() == "Time" {
+		return &openapi3.Schema{Type: &openapi3.Types{"string"}, Format: "date-time"}
+	}
+
+	schema := &openapi3.Schema{
+		Type:       &openapi3.Types{"object"},
+		Properties: make(openapi3.Schemas),
+	}
+
+	var required []string
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+
+		if !field.IsExported() {
+			continue
+		}
+
+		jsonTag := field.Tag.Get("json")
+		if jsonTag == "-" {
+			continue
+		}
+
+		name := field.Name
+		tagParts := strings.Split(jsonTag, ",")
+		if len(tagParts) > 0 && tagParts[0] != "" {
+			name = tagParts[0]
+		}
+
+		isOptional := false
+		for _, part := range tagParts[1:] {
+			if part == "omitempty" {
+				isOptional = true
+				break
+			}
+		}
+
+		fieldSchema := generateSchemaFromType(field.Type)
+
+		if doc := field.Tag.Get("doc"); doc != "" {
+			fieldSchema.Description = doc
+		}
+
+		if ex := field.Tag.Get("example"); ex != "" {
+			fieldSchema.Example = ex
+		}
+
+		schema.Properties[name] = &openapi3.SchemaRef{Value: fieldSchema}
+
+		if !isOptional {
+			required = append(required, name)
+		}
+	}
+
+	if len(required) > 0 {
+		schema.Required = required
+	}
+
+	return schema
+}
+
+func ptr[T any](v T) *T {
+	return &v
+}
---
+++ openapi/route.go
@@ -0,0 +1,387 @@
+package openapi
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/getkin/kin-openapi/openapi3"
+)
+
+type RouteBuilder struct {
+	openapi   *OpenAPI
+	method    string
+	path      string
+	operation *openapi3.Operation
+}
+
+func (rb *RouteBuilder) autoExtractPathParams() {
+	parts := strings.Split(rb.path, "/")
+	for _, part := range parts {
+		var paramName string
+
+		if strings.HasPrefix(part, ":") {
+			paramName = strings.TrimPrefix(part, ":")
+		}
+
+		if strings.HasPrefix(part, "{") && strings.HasSuffix(part, "}") {
+			paramName = strings.TrimSuffix(strings.TrimPrefix(part, "{"), "}")
+		}
+		if paramName != "" {
+			rb.operation.Parameters = append(rb.operation.Parameters, &openapi3.ParameterRef{
+				Value: &openapi3.Parameter{
+					Name:     paramName,
+					In:       "path",
+					Required: true,
+					Schema:   &openapi3.SchemaRef{Value: &openapi3.Schema{Type: &openapi3.Types{"string"}}},
+				},
+			})
+		}
+	}
+}
+
+func (rb *RouteBuilder) Summary(summary string) *RouteBuilder {
+	rb.operation.Summary = summary
+	return rb
+}
+
+func (rb *RouteBuilder) Description(description string) *RouteBuilder {
+	rb.operation.Description = description
+	return rb
+}
+
+func (rb *RouteBuilder) OperationID(id string) *RouteBuilder {
+	rb.operation.OperationID = id
+	return rb
+}
+
+func (rb *RouteBuilder) Tags(tags ...string) *RouteBuilder {
+	rb.operation.Tags = append(rb.operation.Tags, tags...)
+	return rb
+}
+
+func (rb *RouteBuilder) Deprecated(deprecated bool) *RouteBuilder {
+	rb.operation.Deprecated = deprecated
+	return rb
+}
+
+func (rb *RouteBuilder) PathParam(name, description string) *ParamBuilder {
+	param := rb.findOrCreateParam(name, "path")
+	param.Description = description
+	param.Required = true
+	return &ParamBuilder{route: rb, param: param}
+}
+
+func (rb *RouteBuilder) QueryParam(name, description string) *ParamBuilder {
+	param := rb.findOrCreateParam(name, "query")
+	param.Description = description
+	return &ParamBuilder{route: rb, param: param}
+}
+
+func (rb *RouteBuilder) HeaderParam(name, description string) *ParamBuilder {
+	param := rb.findOrCreateParam(name, "header")
+	param.Description = description
+	return &ParamBuilder{route: rb, param: param}
+}
+
+func (rb *RouteBuilder) CookieParam(name, description string) *ParamBuilder {
+	param := rb.findOrCreateParam(name, "cookie")
+	param.Description = description
+	return &ParamBuilder{route: rb, param: param}
+}
+
+func (rb *RouteBuilder) findOrCreateParam(name, in string) *openapi3.Parameter {
+	for _, p := range rb.operation.Parameters {
+		if p.Value != nil && p.Value.Name == name && p.Value.In == in {
+			return p.Value
+		}
+	}
+
+	param := &openapi3.Parameter{
+		Name:   name,
+		In:     in,
+		Schema: &openapi3.SchemaRef{Value: &openapi3.Schema{Type: &openapi3.Types{"string"}}},
+	}
+	rb.operation.Parameters = append(rb.operation.Parameters, &openapi3.ParameterRef{Value: param})
+	return param
+}
+
+func (rb *RouteBuilder) Body(example any, description string) *RouteBuilder {
+	schema := generateSchema(example)
+
+	rb.operation.RequestBody = &openapi3.RequestBodyRef{
+		Value: &openapi3.RequestBody{
+			Description: description,
+			Required:    true,
+			Content: openapi3.Content{
+				"application/json": &openapi3.MediaType{
+					Schema: &openapi3.SchemaRef{Value: schema},
+				},
+			},
+		},
+	}
+	return rb
+}
+
+func (rb *RouteBuilder) BodyOptional(example any, description string) *RouteBuilder {
+	rb.Body(example, description)
+	rb.operation.RequestBody.Value.Required = false
+	return rb
+}
+
+func (rb *RouteBuilder) Response(statusCode int, example any, description string) *RouteBuilder {
+	var content openapi3.Content
+
+	if example != nil {
+		schema := generateSchema(example)
+		content = openapi3.Content{
+			"application/json": &openapi3.MediaType{
+				Schema: &openapi3.SchemaRef{Value: schema},
+			},
+		}
+	}
+
+	rb.operation.Responses.Set(statusCodeToString(statusCode), &openapi3.ResponseRef{
+		Value: &openapi3.Response{
+			Description: &description,
+			Content:     content,
+		},
+	})
+
+	return rb
+}
+
+func (rb *RouteBuilder) ResponseWithHeaders(statusCode int, example any, description string, headers map[string]string) *RouteBuilder {
+	rb.Response(statusCode, example, description)
+
+	resp := rb.operation.Responses.Value(statusCodeToString(statusCode))
+	if resp != nil && resp.Value != nil {
+		resp.Value.Headers = make(openapi3.Headers)
+		for name, desc := range headers {
+			resp.Value.Headers[name] = &openapi3.HeaderRef{
+				Value: &openapi3.Header{
+					Parameter: openapi3.Parameter{
+						Description: desc,
+						Schema:      &openapi3.SchemaRef{Value: &openapi3.Schema{Type: &openapi3.Types{"string"}}},
+					},
+				},
+			}
+		}
+	}
+
+	return rb
+}
+
+func (rb *RouteBuilder) Security(schemes ...string) *RouteBuilder {
+	if rb.operation.Security == nil {
+		rb.operation.Security = &openapi3.SecurityRequirements{}
+	}
+	for _, scheme := range schemes {
+		req := openapi3.SecurityRequirement{}
+		req[scheme] = []string{}
+		*rb.operation.Security = append(*rb.operation.Security, req)
+	}
+	return rb
+}
+
+func (rb *RouteBuilder) SecurityAll(schemes ...string) *RouteBuilder {
+	if rb.operation.Security == nil {
+		rb.operation.Security = &openapi3.SecurityRequirements{}
+	}
+	req := openapi3.SecurityRequirement{}
+	for _, scheme := range schemes {
+		req[scheme] = []string{}
+	}
+	*rb.operation.Security = append(*rb.operation.Security, req)
+	return rb
+}
+
+func (rb *RouteBuilder) SecurityWithScopes(scheme string, scopes ...string) *RouteBuilder {
+	if rb.operation.Security == nil {
+		rb.operation.Security = &openapi3.SecurityRequirements{}
+	}
+	req := openapi3.SecurityRequirement{}
+	req[scheme] = scopes
+	*rb.operation.Security = append(*rb.operation.Security, req)
+	return rb
+}
+
+func (rb *RouteBuilder) NoSecurity() *RouteBuilder {
+	rb.operation.Security = &openapi3.SecurityRequirements{}
+	return rb
+}
+
+func (rb *RouteBuilder) ExternalDocs(url, description string) *RouteBuilder {
+	rb.operation.ExternalDocs = &openapi3.ExternalDocs{
+		URL:         url,
+		Description: description,
+	}
+	return rb
+}
+
+func (rb *RouteBuilder) Build() {
+	rb.openapi.addOperation(rb.method, rb.path, rb.operation)
+}
+
+func statusCodeToString(code int) string {
+	switch code {
+	case http.StatusOK:
+		return "200"
+	case http.StatusCreated:
+		return "201"
+	case http.StatusAccepted:
+		return "202"
+	case http.StatusNoContent:
+		return "204"
+	case http.StatusBadRequest:
+		return "400"
+	case http.StatusUnauthorized:
+		return "401"
+	case http.StatusForbidden:
+		return "403"
+	case http.StatusNotFound:
+		return "404"
+	case http.StatusMethodNotAllowed:
+		return "405"
+	case http.StatusConflict:
+		return "409"
+	case http.StatusUnprocessableEntity:
+		return "422"
+	case http.StatusTooManyRequests:
+		return "429"
+	case http.StatusInternalServerError:
+		return "500"
+	case http.StatusBadGateway:
+		return "502"
+	case http.StatusServiceUnavailable:
+		return "503"
+	default:
+		return strconv.Itoa(code)
+	}
+}
+
+type ParamBuilder struct {
+	route *RouteBuilder
+	param *openapi3.Parameter
+}
+
+func (pb *ParamBuilder) Required() *ParamBuilder {
+	pb.param.Required = true
+	return pb
+}
+
+func (pb *ParamBuilder) Optional() *ParamBuilder {
+	pb.param.Required = false
+	return pb
+}
+
+func (pb *ParamBuilder) Type(t string) *ParamBuilder {
+	pb.param.Schema.Value.Type = &openapi3.Types{t}
+	return pb
+}
+
+func (pb *ParamBuilder) TypeInt() *ParamBuilder {
+	pb.param.Schema.Value.Type = &openapi3.Types{"integer"}
+	return pb
+}
+
+func (pb *ParamBuilder) TypeBool() *ParamBuilder {
+	pb.param.Schema.Value.Type = &openapi3.Types{"boolean"}
+	return pb
+}
+
+func (pb *ParamBuilder) TypeNumber() *ParamBuilder {
+	pb.param.Schema.Value.Type = &openapi3.Types{"number"}
+	return pb
+}
+
+func (pb *ParamBuilder) Format(format string) *ParamBuilder {
+	pb.param.Schema.Value.Format = format
+	return pb
+}
+
+func (pb *ParamBuilder) Enum(values ...string) *ParamBuilder {
+	for _, v := range values {
+		pb.param.Schema.Value.Enum = append(pb.param.Schema.Value.Enum, v)
+	}
+	return pb
+}
+
+func (pb *ParamBuilder) Default(value any) *ParamBuilder {
+	pb.param.Schema.Value.Default = value
+	return pb
+}
+
+func (pb *ParamBuilder) Example(value any) *ParamBuilder {
+	pb.param.Example = value
+	return pb
+}
+
+func (pb *ParamBuilder) Min(min float64) *ParamBuilder {
+	pb.param.Schema.Value.Min = &min
+	return pb
+}
+
+func (pb *ParamBuilder) Max(max float64) *ParamBuilder {
+	pb.param.Schema.Value.Max = &max
+	return pb
+}
+
+func (pb *ParamBuilder) MinLength(length uint64) *ParamBuilder {
+	pb.param.Schema.Value.MinLength = length
+	return pb
+}
+
+func (pb *ParamBuilder) MaxLength(length *uint64) *ParamBuilder {
+	pb.param.Schema.Value.MaxLength = length
+	return pb
+}
+
+func (pb *ParamBuilder) Pattern(pattern string) *ParamBuilder {
+	pb.param.Schema.Value.Pattern = pattern
+	return pb
+}
+
+func (pb *ParamBuilder) Done() *RouteBuilder {
+	return pb.route
+}
+
+func (pb *ParamBuilder) Summary(summary string) *RouteBuilder {
+	return pb.route.Summary(summary)
+}
+
+func (pb *ParamBuilder) Description(description string) *RouteBuilder {
+	return pb.route.Description(description)
+}
+
+func (pb *ParamBuilder) Tags(tags ...string) *RouteBuilder {
+	return pb.route.Tags(tags...)
+}
+
+func (pb *ParamBuilder) PathParam(name, description string) *ParamBuilder {
+	return pb.route.PathParam(name, description)
+}
+
+func (pb *ParamBuilder) QueryParam(name, description string) *ParamBuilder {
+	return pb.route.QueryParam(name, description)
+}
+
+func (pb *ParamBuilder) HeaderParam(name, description string) *ParamBuilder {
+	return pb.route.HeaderParam(name, description)
+}
+
+func (pb *ParamBuilder) Body(example any, description string) *RouteBuilder {
+	return pb.route.Body(example, description)
+}
+
+func (pb *ParamBuilder) Response(statusCode int, example any, description string) *RouteBuilder {
+	return pb.route.Response(statusCode, example, description)
+}
+
+func (pb *ParamBuilder) Security(schemes ...string) *RouteBuilder {
+	return pb.route.Security(schemes...)
+}
+
+func (pb *ParamBuilder) Build() {
+	pb.route.Build()
+}
